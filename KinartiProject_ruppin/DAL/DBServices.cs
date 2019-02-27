6e65043a556bc42defccd41946806526a23eab29
AinavBS <46422639+AinavBS@users.noreply.github.com>
2019-02-27 19:43:13 +0200

    create DBService

---
+++ KinartiProject_ruppin/DAL/DBServices.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+using System.Data;
+using System.Text;
+using WebApplication1.Models;
+
+namespace KinartiProject_ruppin.DAL
+{
+    public class DBServices
+    {
+        public SqlDataAdapter da;
+        public DataTable dt;
+
+        public DBservices()
+        {
+        }
+
+        //--------------------------------------------------------------------------------------------------
+        // creates a connection to the database according to the connectionString name in the web.config 
+        //--------------------------------------------------------------------------------------------------
+        public SqlConnection connect(String conString)
+        {
+            // read the connection string from the configuration file
+            string cStr = WebConfigurationManager.ConnectionStrings[conString].ConnectionString;
+            SqlConnection con = new SqlConnection(cStr);
+            con.Open();
+            return con;
+        }
+
+        //---------------------------------------------------------------------------------
+        // Create the SqlCommand
+        //---------------------------------------------------------------------------------
+        private SqlCommand CreateCommand(String CommandSTR, SqlConnection con)
+        {
+            SqlCommand cmd = new SqlCommand(); // create the command object
+            cmd.Connection = con;              // assign the connection to the command object
+            cmd.CommandText = CommandSTR;      // can be Select, Insert, Update, Delete 
+            cmd.CommandTimeout = 10;           // Time to wait for the execution' The default is 30 seconds
+            cmd.CommandType = System.Data.CommandType.Text; // the type of the command, can also be stored procedure
+            return cmd;
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
