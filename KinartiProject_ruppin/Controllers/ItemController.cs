31bae7442d1a2b8bbfe387426c27794256321c6d
Alex-Tochilovsky <alex.tochilovsky@gmail.com>
2019-02-28 18:58:25 +0200

    Added ItemController Added function to get the items from project to DBServices Added Function to get items from project in Items.cs
    Added couple of rows to the DB as well(Project\Item\Status).

---
+++ KinartiProject_ruppin/Controllers/ItemController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using KinartiProject_ruppin.Models;
+
+namespace KinartiProject_ruppin.Controllers
+{
+    public class ItemController : ApiController
+    {
+        [HttpGet]
+        [Route("api/GetProjectItems")]
+        public IEnumerable<Item> Get(float projNum)
+        {
+            Item I = new Item();
+            List<Item> ItemList = new List<Item>();
+            ItemList = I.GetProjectItems(projNum);
+            return ItemList;
+        }
+    }
+}
---
+++ KinartiProject_ruppin/Models/Item.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KinartiProject_ruppin.Models
+{
+    public class Item
+    {
+        public string ItemNum { get; set; }
+        public string ItemName { get; set; }
+        public string ItemStatus { get; set; }
+        public double ItemCompletedPercentage { get; set; }
+        public int ItemGroupCount { get; set; }
+
+        public Item(string itemnum, string itemname, string itemstatus, double itemcompletedpercentage, int itemgroupcount)
+        {
+            ItemNum = itemnum;
+            ItemName = itemname;
+            ItemStatus = itemstatus;
+            ItemCompletedPercentage = itemcompletedpercentage;
+            ItemGroupCount = itemgroupcount;
+        }
+        public Item()
+        {
+
+        }
+
+        public List<Item> GetProjectItems(float projNum)
+        {
+            DBServices dbs = new DBServices();
+            List<Item> Pi = new List<Item>();
+            Pi = dbs.GetProjectItems(projNum);
+            return Pi;
+        }
+    }
+}
