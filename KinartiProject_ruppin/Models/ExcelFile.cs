7c0c690c770433640f152783ebc7b7b5013751dc
Alex-Tochilovsky <alex.tochilovsky@gmail.com>
2019-04-07 23:19:35 +0300

    Done with the Exceptions for the Excel File Upload.
    Need to update the swal for a nice view of the exception.

--- KinartiProject_ruppin/Models/ExcelFile.cs
+++ KinartiProject_ruppin/Models/ExcelFile.cs
@@ -3,9 +3,11 @@ using System.IO;
 using System.Diagnostics;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
+using System.Net.Http;
 using System.Linq;
 using System.Web;
 using Excel = Microsoft.Office.Interop.Excel;
+using Microsoft.CSharp.RuntimeBinder;
 
 namespace KinartiProject_ruppin.Models
 {
@@ -31,18 +33,22 @@ namespace KinartiProject_ruppin.Models
 
         public void WorkOnExcelFile(string filename, string fileuploaddate)
         {
+            string path = @"C:\Users\alex.tochilovsky\source\repos\KinartiProject_ruppin\KinartiProject_ruppin\" + filename;
             List<Part> PartList = new List<Part>();
             string temp1 = "";
             List<string> temp = new List<string>();
             Excel.Application excelApp = new Excel.Application();
-            Excel.Workbook excelBook = excelApp.Workbooks.Open(@"C:\Users\user\source\repos\KinartiProject_\KinartiProject_ruppin\" + filename);
+            Excel.Workbook excelBook = excelApp.Workbooks.Open(path);
             Excel._Worksheet excelSheet = excelBook.Sheets[1];
             Excel.Range excelRange = excelSheet.UsedRange;
-            
+
 
             // Calculating rows and cols.
             int rowCount = excelRange.Rows.Count;
             int colCount = excelRange.Columns.Count;
+
+            //get the excel process ID - for future delete
+            var ExcelIdProcess = GetExcelProcess(excelApp);
 
             //Reading step by step cols and rows.
             for (int i = 2; i <= rowCount; i++)
@@ -62,94 +68,156 @@ namespace KinartiProject_ruppin.Models
                             temp1 = excelRange.Cells[i, j].Value2.ToString();
                         }
                     }
-                    // Inside the case we can change the Convert.ToInt32(excelRange.Cells[i, j].Value2); values to temp1.....
-                    switch (excelRange.Cells[1, j].Value2.ToString())
-                    {
-                        case "מספר_ארגז":
-                            part.PartSetNumber = Convert.ToInt32(temp1);
-                            break;
-                        case "מספר_חלק":
-                            part.PartNum = temp1;
-                            break;
-                        case "שם_חלק":
-                            part.PartName = temp1;
-                            break;
-                        case "קנטים":
-                            part.PartKantim = temp1;
-                            break;
-                        case "פעולת_מכונה_ראשונה":
-                            part.PartFirstMachine = temp1;
-                            break;
-                        case "פעולת_מכונת_שניה":
-                            part.PartSecondMachine = temp1;
-                            break;
-                        case "חומר":
-                            part.PartMaterial = temp1;
-                            break;
-                        case "צבע_":
-                            part.PartColor = temp1;
-                            break;
-                        case "אורך_חלק":
-                            part.PartLength = Convert.ToInt32(temp1);
-                            break;
-                        case "רוחב_חלק":
-                            part.PartWidth = Convert.ToInt32(temp1);
-                            break;
-                        case "עובי":
-                            part.PartThickness = Convert.ToInt32(temp1);
-                            break;
-                        case "תוספת_לאורך":
-                            part.AdditionToLength = Convert.ToInt32(temp1);
-                            break;
-                        case "תוספת_לרוחב":
-                            part.AdditionToWidth = Convert.ToInt32(temp1);
-                            break;
-                        case "תוספת_לעובי":
-                            part.AdditionToThickness = Convert.ToInt32(temp1);
-                            break;
-                        //case "תז_של_חלק":
-                        //    part.PartID = temp1;
-                        //    break;
-                        case "בר_קוד_תז":
-                            part.PartBarCode = temp1;
-                            break;
-                        case "מכונת_חיתוך":
-                            part.PartCropType = temp1;
-                            break;
-                        case "קטגוריית_חלק":
-                            part.PartCategory = temp1;
-                            break;
-                        case "הערות":
-                            part.PartComment = temp1;
-                            break;
-                        case "כמות":
-                            part.PartQuantity = Convert.ToInt32(temp1);
-                            break;
 
-                        default:
-                            break;
-                            // code block
+                    // Inside the case we can change the Convert.ToInt32(excelRange.Cells[i, j].Value2); values to temp1.....
+                    try
+                    {
+                        switch (excelRange.Cells[1, j].Value2.ToString())
+                        {
+                            case "מספר_ארגז":
+                                part.PartSetNumber = Convert.ToInt32(temp1);
+                                break;
+                            case "מספר_חלק":
+                                part.PartNum = temp1;
+                                break;
+                            case "שם_חלק":
+                                part.PartName = temp1;
+                                break;
+                            case "קנטים":
+                                part.PartKantim = temp1;
+                                break;
+                            case "פעולת_מכונה_ראשונה":
+                                part.PartFirstMachine = temp1;
+                                break;
+                            case "פעולת_מכונת_שניה":
+                                part.PartSecondMachine = temp1;
+                                break;
+                            case "חומר":
+                                part.PartMaterial = temp1;
+                                break;
+                            case "צבע_":
+                                part.PartColor = temp1;
+                                break;
+                            case "אורך_חלק":
+                                part.PartLength = Convert.ToInt32(temp1);
+                                break;
+                            case "רוחב_חלק":
+                                part.PartWidth = Convert.ToInt32(temp1);
+                                break;
+                            case "עובי":
+                                part.PartThickness = Convert.ToInt32(temp1);
+                                break;
+                            case "תוספת_לאורך":
+                                part.AdditionToLength = Convert.ToInt32(temp1);
+                                break;
+                            case "תוספת_לרוחב":
+                                part.AdditionToWidth = Convert.ToInt32(temp1);
+                                break;
+                            case "תוספת_לעובי":
+                                part.AdditionToThickness = Convert.ToInt32(temp1);
+                                break;
+                            //case "תז_של_חלק":
+                            //    part.PartID = temp1;
+                            //    break;
+                            case "בר_קוד_תז":
+                                part.PartBarCode = temp1;
+                                break;
+                            case "מכונת_חיתוך":
+                                part.PartCropType = temp1;
+                                break;
+                            case "קטגוריית_חלק":
+                                part.PartCategory = temp1;
+                                break;
+                            case "הערות":
+                                part.PartComment = temp1;
+                                break;
+                            case "כמות":
+                                part.PartQuantity = Convert.ToInt32(temp1);
+                                break;
+
+                            default:
+                                break;
+                                // code block
+                        }
                     }
+
+                    //שגיאת כאשר הוא מזהה שאחד הכותרות קבל ערך נאל
+                    catch (RuntimeBinderException e)
+                    { 
+                        excelBook.Close();
+                        excelApp.Quit();
+                        System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
+                        KillSpecificExcelFileProcess(ExcelIdProcess.Id);
+                        File.Delete(path);
+                        throw (e);
+                    }
+                    //אקספשן כאשר יש שדה עם טייפ לא נכון שמנסים להכניס
+                    catch(FormatException e)
+                    {                      
+                        excelBook.Close();
+                        excelApp.Quit();
+                        System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
+                        KillSpecificExcelFileProcess(ExcelIdProcess.Id);
+                        File.Delete(path);
+                        throw (e);
+                    }
+                    catch(Exception e)
+                    {
+
+                    }
+                    part.PartStatus = "חלק טרם נסרק";
+                    part.GroupName = "";
                 }
-                part.PartStatus = "חלק טרם נסרק";
-                part.GroupName = "";
                 PartList.Add(part);
             }
-            var ExcelIdProcess = GetExcelProcess(excelApp);
+
             Item Item = new Item(excelRange.Cells[2, 3].Value2.ToString(), PartList);
             try
             {
                 Project NewData = new Project(Convert.ToSingle(excelRange.Cells[2, 1].Value2), excelRange.Cells[2, 2].Value2, fileuploaddate, Item);
             }
+
+            //כאשר נכנס קובץ אקסל עם ערכים לא נכונים זה קופץ
+            catch (System.Data.SqlClient.SqlException e)
+            {
+                //שגיאת מפתחות - הכנסה של נתונים קיימים
+                if (e.Number == 2627)
+                {
+                    throw new DuplicatePrimaryKeyException(e.Message);
+                }
+                else
+                {
+                    KillSpecificExcelFileProcess(ExcelIdProcess.Id);
+                    File.Delete(path);
+                    throw (e);
+                }
+            }
+
+            //שגיאת מפתחות - הכנסה של נתונים קיימים
+            catch (DuplicatePrimaryKeyException e)
+            {
+                KillSpecificExcelFileProcess(ExcelIdProcess.Id);
+                File.Delete(path);
+                throw (e);
+            }
+
+            //כאשר מנסים לעלות קובץ שהוא לא קובץ אקסל
+            catch (RuntimeBinderException e)
+            {
+                KillSpecificExcelFileProcess(ExcelIdProcess.Id);
+                File.Delete(path);
+                throw (e);
+            }
+            // כל שגיאה כללית אחרת
             catch (Exception e)
             {
                 KillSpecificExcelFileProcess(ExcelIdProcess.Id);
-                string excelFileName = filename.Replace(@"uploadedFiles\", "");
-                File.Delete(@"C:\Users\user\source\repos\KinartiProject_\KinartiProject_ruppin\uploadedFiles\" + excelFileName);
+                File.Delete(path);
                 throw (e);
             }
-            
+
             //after reading, relaase the excel project
+            KillSpecificExcelFileProcess(ExcelIdProcess.Id);
             excelApp.Quit();
             System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
         }
---
+++ KinartiProject_ruppin/Models/MyExceptions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KinartiProject_ruppin.Models
+{
+    [Serializable]
+    public class DuplicatePrimaryKeyException : Exception
+    {
+        public DuplicatePrimaryKeyException()
+        { }
+
+        public DuplicatePrimaryKeyException(string message)
+            : base(message)
+        { }
+
+        public DuplicatePrimaryKeyException(string message, Exception innerException)
+            : base(message, innerException)
+        { }
+    }
+}
